edc45d1f77cda1ddfe7aea89d92650caa51b9380
harbinzhang <seashore.real@gmail.com>
2018-11-02 21:46:36 -0700

    auto

---
+++ kafka/produce.go
@@ -0,0 +1,57 @@
+package kafka
+
+import (
+	"fmt"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func produce(lagQueue chan LagMessage) {
+	kafkaConfig := kafka.ConfigMap{
+		"batch.num.messages": 2000,
+		"linger.ms":          1,
+		"bootstrap.servers":  "speed-racer-kafka.zeta.tools",
+		// "buffer.memory=33554432"
+		"socket.send.buffer.bytes": 1024000,
+		// "reconnect.backoff.ms":     100,
+		// "block.on.buffer.full=true"
+		"retries":            6,
+		"retry.backoff.ms":   100,
+		"compression.type":   "gzip",
+		"request.timeout.ms": 900000,
+	}
+	p, err := kafka.NewProducer(&kafkaConfig)
+	if err != nil {
+		panic(err)
+	}
+
+	defer p.Close()
+
+	// Delivery report handler for produced messages
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
+				} else {
+					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
+				}
+			}
+		}
+	}()
+
+	message := "fjord.burrow.test3.python-consumer-1.BusinessEvent.0.maxLag 0.00 1541214139 source=192.168.3.169 data_center=slv dca_zone=local department=fjord planet=sbx888 service_name=porter_rainbow porter_tools=porter-rainbow"
+
+	// Produce messages to topic (asynchronously)
+	topic := "micrometer-wavefront"
+	for _, word := range []string{message} {
+		p.Produce(&kafka.Message{
+			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+			Value:          []byte(word),
+		}, nil)
+	}
+
+	// Wait for message deliveries before shutting down
+	p.Flush(15 * 1000)
+}

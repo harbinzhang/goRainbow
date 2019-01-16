3f69e32e1ff5962ea443b3f1c8e6851f3d4bf319
harbinzhang <seashore.real@gmail.com>
2019-01-16 10:27:53 -0800

    auto

--- core/utils/twinStateMachine.go
+++ core/utils/twinStateMachine.go
@@ -45,10 +45,11 @@ func (tsm *TwinStateMachine) Put(key string, lag int) bool {
 	}
 }
 
-// PartitionPut is for Partition level use, it's different from general put
+// PartitionPut is for Partition level use, it's different from Put:
+// 1. It would keep not sending metrics if lag is 0.
+// 2. It should send the previous lag(if==0) if current lag != 0 to make sure
 // in Partition level, it would keep not sending metrics if lag is 0.
-// Also it should send the previous lag=0 if current lag != 0 to make sure
-// all partition metrics start from 0.
+// 3. No need to adjust traffic rate, because already controlled in Put(totalLag).
 //
 // Using >0, -1, -2 to store 3 states in twoStateMachine.
 // @params
@@ -62,8 +63,6 @@ func (tsm *TwinStateMachine) PartitionPut(key string, lag int) (bool, bool) {
 	if val, ok := tsm.mmap[key]; ok {
 		if lag == 0 && val == 0 {
 			// skip this lag
-		} else if lag == val {
-			tsm.mmap[key] = -1
 		} else if lag == 0 && val > 0 {
 			tsm.mmap[key] = -2
 		} else {
@@ -77,31 +76,6 @@ func (tsm *TwinStateMachine) PartitionPut(key string, lag int) (bool, bool) {
 		tsm.mmap[key] = lag
 		shouldSendCurrentLag = true
 	}
-
-	// It's the origin logic before simplification
-	// if val, ok := tsm.mmap[key]; ok {
-	// 	if lag == 0 && val == 0 {
-	// 		return false, false
-	// 	} else if lag == val {
-	// 		tsm.mmap[key] = -1
-	// 		return false, false
-	// 	} else if lag == 0 && val > 0 {
-	// 		tsm.mmap[key] = -2
-	// 		return false, false
-	// 	} else if lag == 0 && val == -2 {
-	// 		tsm.mmap[key] = lag
-	// 		return true, false
-	// 	} else if lag > 0 && val == 0 {
-	// 		tsm.mmap[key] = lag
-	// 		return true, true
-	// 	} else {
-	// 		tsm.mmap[key] = lag
-	// 		return true, false
-	// 	}
-	// } else {
-	// 	tsm.mmap[key] = lag
-	// 	return true, false
-	// }
 
 	return shouldSendCurrentLag, shouldSendPreviousLag
 }

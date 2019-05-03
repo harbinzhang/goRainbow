49d461550f285dc8dd9ecf55c4b79a2659114359
hzhang <hzhang@zuora.com>
2019-05-03 07:47:39 -0700

    config log level

--- core/util/loggerProvider.go
+++ core/util/loggerProvider.go
@@ -3,15 +3,13 @@ package util
 import (
 	"encoding/json"
 
-	"go.uber.org/zap/zapcore"
-
 	"go.uber.org/zap"
 )
 
 // GetLogger returns a zap.Logger, need to take care of Sync() before exiting.
 func GetLogger() *zap.Logger {
 	rawJSON := []byte(`{
-		"level": "warn",
+		"level": "debug",
 		"encoding": "json",
 		"outputPaths": ["./rainbow_log"],
 		"errorOutputPaths": ["stderr"],
@@ -27,8 +25,6 @@ func GetLogger() *zap.Logger {
 	if err := json.Unmarshal(rawJSON, &cfg); err != nil {
 		panic("Err decode logger config: " + err.Error())
 	}
-
-	cfg.Level.SetLevel(zapcore.DebugLevel)
 
 	logger, err := cfg.Build()
 	if err != nil {
